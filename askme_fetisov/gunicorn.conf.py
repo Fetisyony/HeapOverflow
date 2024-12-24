6093da409596b12ffa4f56dec0575cc8dacbc739
Fetisov Igor <fetisyony@gmail.com>
2024-12-24 14:06:13 +0300

    HW6: set up wsgi

---
+++ askme_fetisov/gunicorn.conf.py
@@ -0,0 +1,12 @@
+import multiprocessing
+
+
+workers = 2
+
+bind = "127.0.0.1:8081"
+
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
+
+timeout = 30
--- askme_fetisov/wsgi.py
+++ askme_fetisov/wsgi.py
@@ -7,10 +7,40 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+from io import BytesIO
 import os
 
 from django.core.wsgi import get_wsgi_application
+from urllib.parse import parse_qs
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askme_fetisov.settings')
 
-application = get_wsgi_application()
+application_core = get_wsgi_application()
+
+def application(environ, start_response):
+    get_params = parse_qs(environ.get('QUERY_STRING', ''))
+
+    try:
+        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+        request_body = environ['wsgi.input'].read(request_body_size)
+    except (ValueError, TypeError):
+        request_body = b""
+
+    environ['wsgi.input'] = BytesIO(request_body)
+
+    print("============================")
+    print("GET params:")
+    print(get_params)
+    if request_body:
+        try:
+            post_data = request_body.decode('utf-8')
+            print("POST params:")
+            print(post_data)
+        except UnicodeDecodeError:
+            print("POST data is not UTF-8 encoded.")
+    else:
+        print("No POST params")
+    print("============================")
+
+    return application_core(environ, start_response)
+
